921999c34f3d8e9c7f6fce0d61018ad1bcf10fbb
Emilian Nemec <emiliannemec@gmx.de>
2022-11-22 12:44:48 +0100

    WS-55: Html tag solution

--- src/Playground/Controls/components/PlaygroundUI.tsx
+++ src/Playground/Controls/components/PlaygroundUI.tsx
@@ -1,6 +1,15 @@
+import { Html } from "@react-three/drei";
+
 function PlaygroundUI() {
     return ( <>
-    
+    {/**
+     * using <Html from drei, because you cant create a portal inside the Canvas, that renders outside
+     */}
+        <Html as="div">
+            <div>
+                hi
+            </div>
+        </Html>
     </> );
 }
 
