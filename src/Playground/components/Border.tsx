05118c971a68369c48397c0f86e6bd6b87a503b9
Emilian Nemec <emiliannemec@gmx.de>
2023-01-02 07:22:52 +0100

    WS-94: resolved pipeline issues

--- src/Playground/components/Border.tsx
+++ src/Playground/components/Border.tsx
@@ -2,37 +2,34 @@ import { Box } from '@react-three/drei';
 
 function Border(): JSX.Element {
     return (
-        <>
+        <group>
+            <mesh receiveShadow castShadow>
+                <boxGeometry args={[20, 0.2, 20, 4, 4, 4]} />
+                <meshPhongMaterial color="green" />
+            </mesh>
             <group>
-                <mesh receiveShadow castShadow>
-                    <boxGeometry args={[20, 0.2, 20, 4, 4, 4]} />
-                    <meshPhongMaterial color="green" />
-                </mesh>
-
-                <group>
-                    <Box
-                        args={[20, 4, 0.2, 4, 4, 4]}
-                        position={[10, 0, 0]}
-                        rotation={[0, Math.PI / 2, 0]}
-                    >
-                        <meshPhongMaterial color="grey" />
-                    </Box>
-                    <Box
-                        args={[20, 4, 0.2, 4, 4, 4]}
-                        position={[-10, 0, 0]}
-                        rotation={[0, Math.PI / 2, 0]}
-                    >
-                        <meshPhongMaterial color="grey" />
-                    </Box>
-                    <Box args={[20, 4, 0.2, 4, 4, 4]} position={[0, 0, 10]}>
-                        <meshPhongMaterial color="grey" />
-                    </Box>
-                    <Box args={[20, 4, 0.2, 4, 4, 4]} position={[0, 0, -10]}>
-                        <meshPhongMaterial color="grey" />
-                    </Box>
-                </group>
+                <Box
+                    args={[20, 4, 0.2, 4, 4, 4]}
+                    position={[10, 0, 0]}
+                    rotation={[0, Math.PI / 2, 0]}
+                >
+                    <meshPhongMaterial color="grey" />
+                </Box>
+                <Box
+                    args={[20, 4, 0.2, 4, 4, 4]}
+                    position={[-10, 0, 0]}
+                    rotation={[0, Math.PI / 2, 0]}
+                >
+                    <meshPhongMaterial color="grey" />
+                </Box>
+                <Box args={[20, 4, 0.2, 4, 4, 4]} position={[0, 0, 10]}>
+                    <meshPhongMaterial color="grey" />
+                </Box>
+                <Box args={[20, 4, 0.2, 4, 4, 4]} position={[0, 0, -10]}>
+                    <meshPhongMaterial color="grey" />
+                </Box>
             </group>
-        </>
+        </group>
     );
 }
 
