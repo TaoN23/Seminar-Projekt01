a215a6cf9eb56c618aaeb15f12e850dc48ac7c26
Emilian Nemec <emiliannemec@gmx.de>
2023-01-03 20:43:05 +0100

    WS-13: Sphere added

--- src/Game/Player/components/PlayerControls.tsx
+++ src/Game/Player/components/PlayerControls.tsx
@@ -1,6 +1,19 @@
+import { useSphere } from '@react-three/cannon';
+import { SphereGeometryProps } from '@react-three/fiber';
+import { Mesh, Object3D, Sphere, SphereGeometry } from 'three';
+
 function PlayerControls(): JSX.Element {
-    // eslint-disable-next-line react/jsx-no-useless-fragment
-    return <></>;
+    const [playerBoundingboxRef] = useSphere<Mesh>(() => ({
+        mass: 1,
+        args: [0.5],
+        position: [-2, 1, 0],
+    }));
+    return (
+        <mesh ref={playerBoundingboxRef}>
+            <sphereGeometry args={[0.5]} attach="geometry" />
+            <meshBasicMaterial attach="material" />
+        </mesh>
+    );
 }
 
 export default PlayerControls;
