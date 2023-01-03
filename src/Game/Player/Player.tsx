2997575158066ae51a0c3b92d79e1ff236f08d22
Emilian Nemec <emiliannemec@gmx.de>
2023-01-03 16:08:15 +0100

    WS-13: physics ground added

--- src/Game/Player/Player.tsx
+++ src/Game/Player/Player.tsx
@@ -3,12 +3,7 @@ import { useFrame, useThree } from '@react-three/fiber';
 import PlayerControls from './components/PlayerControls';
 
 function Player(): JSX.Element {
-    return (
-        <>
-            <PointerLockControls />
-            <PlayerControls />
-        </>
-    );
+    return <PlayerControls />;
 }
 
 export default Player;
--- src/Playground/components/Border.tsx
+++ src/Playground/components/Border.tsx
@@ -1,11 +1,16 @@
 import { Box } from '@react-three/drei';
+import { usePlane } from '@react-three/cannon';
 import React from 'react';
+import { Mesh } from 'three';
 
 function Border(): JSX.Element {
+    const [groundRef] = usePlane<Mesh>(() => ({
+        rotation: [-Math.PI / 2, 0, 0],
+    }));
     return (
         <group>
-            <mesh receiveShadow castShadow>
-                <boxGeometry args={[20, 0.2, 20, 4, 4, 4]} />
+            <mesh ref={groundRef} receiveShadow castShadow>
+                <planeGeometry args={[20, 20, 4, 4]} />
                 <meshPhongMaterial color="green" />
             </mesh>
 
