9ee9df399ace3cbc21dd08272086929ec70f4023
Emilian Nemec <emiliannemec@gmx.de>
2023-01-04 05:51:42 +0100

    WS-13: updated Shadow maps

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { PointerLockControls, Sky } from '@react-three/drei';
 import { Canvas, useThree } from '@react-three/fiber';
+import { PCFSoftShadowMap, ShadowMapType } from 'three';
 import Game from './Game/Game';
 
 function App(): JSX.Element {
     return (
-        <Canvas shadows>
+        <Canvas shadows={{ type: PCFSoftShadowMap }}>
             <Sky />
             <Game />
         </Canvas>
