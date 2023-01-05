40df6caf5e351fd6b1ddb3911126c86cd95da9bb
Emilian Nemec <emiliannemec@gmx.de>
2023-01-05 04:03:14 +0100

    WS-52: Raycaster works & decals are spawning

--- src/Game/Game.tsx
+++ src/Game/Game.tsx
@@ -1,11 +1,14 @@
 import { Physics } from '@react-three/cannon';
 import { PointerLockControls } from '@react-three/drei';
 import { useThree } from '@react-three/fiber';
+import { useEffect, useState } from 'react';
 import Playground from '../Playground/Playground';
+import Bullethole from './Player/components/Bullethole';
 import Player from './Player/Player';
 
 function Game(): JSX.Element {
     const { camera } = useThree();
+    const [click, setClick] = useState(0);
     return (
         <>
             <Physics gravity={[0, -10, 0]}>
@@ -13,6 +16,7 @@ function Game(): JSX.Element {
                 <Player />
             </Physics>
             <PointerLockControls camera={camera} />
+            <Bullethole />
         </>
     );
 }
