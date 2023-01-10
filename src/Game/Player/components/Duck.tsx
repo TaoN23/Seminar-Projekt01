2e33d2a7a1828860a0f2cb3733b9f1a0eddbef52
TaoN23 <88403373+TaoN23@users.noreply.github.com>
2023-01-10 14:17:31 +0000

    WS-123: duck file added

---
+++ src/Game/Player/components/Duck.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/jsx-no-useless-fragment */
+
+import { useKeyboard } from "../../../config/features/hooks/useKeyboard";
+
+function Duck(): JSX.Element {
+
+    const isPressed = useKeyboard([{key: }])
+    
+    return <></>;
+}
+
+export default Duck;
