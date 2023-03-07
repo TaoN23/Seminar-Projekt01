673edcbc5a4a5629376a34748ce724a8da1d9c35
13Anonym <bauer.michael2005@gmail.com>
2023-03-07 14:21:44 +0100

    WS-123: Pipeline

--- src/Game/Player/components/Duck.tsx
+++ src/Game/Player/components/Duck.tsx
@@ -1,11 +1,9 @@
 /* eslint-disable react/jsx-no-useless-fragment */
 
-import { useKeyboard } from "../../../config/features/hooks/useKeyboard";
+import { useKeyboard } from '../../../config/features/hooks/useKeyboard';
 
 function Duck(): JSX.Element {
-
     //  const isPressed = useKeyboard([{key: }])
-    
     return <></>;
 }
 
